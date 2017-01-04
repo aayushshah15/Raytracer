e5f665ca8af3b558bfc1e4fc67744e3562252254
aayushshah15 <aayush.shah15@gmail.com>
2017-01-04 01:50:35 -0500

    multiple objects

--- sphere.h
+++ sphere.h
@@ -20,7 +20,7 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
 	float discriminant = b*b - a*c;
 	if (discriminant > 0) {
 		float temp = (-b - sqrt(b*b - a*c))/a;
-		if (temp <= t_max && temp => t_min) {
+		if (temp <= t_max && temp >= t_min) {
 			rec.t = temp;
 			rec.p = r.point_at_parameter(rec.t);
 			rec.normal = (rec.p - center) / radius;
